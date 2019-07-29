a1720bfc2d29c478430076bf8e515b7d587e531e
CedszxcJulian <40488618+CedszxcJulian@users.noreply.github.com>
2019-07-29 10:58:24 +0800

    update v1

---
+++ src/user_components/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PageHeader } from 'antd';
+
+function login() {
+    return (
+        <div>
+            <PageHeader onBack={() => null} title="Title" subTitle="This is a subtitle" />,
+            mountNode,
+        </div>
+    );
+}
+
+export default login;
